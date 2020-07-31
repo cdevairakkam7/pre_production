f55fb116dbef0a504c42dd25a75f47d272470e0c
cdevairakkam7 <chris@olinaada.com>
2020-07-30 18:06:39 -0700

    Fonts and Validity

---
+++ static/js/validity_function.js
@@ -0,0 +1,11 @@
+function validity_function(){
+    document.querySelector('.validity').style.display = "flex";
+
+}
+
+function invalid_function(){
+    document.querySelector('.validity').style.display = "none";
+}
+
+
+
